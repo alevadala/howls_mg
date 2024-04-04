17cd4e967a9e7a274725cd54a8e3c94918e9d153
Alessandro Vadala <vadala.alessandro10@gmail.com>
2024-04-04 15:07:34 +0200

    Added changelog

--- dustgrain_2pcf.py
+++ dustgrain_2pcf.py
@@ -1,3 +1,13 @@
+##################################################################
+## CHANGES:
+## Removed plot for individual maps and for xi(theta), just of theta^2.xi(theta).
+## Changed l range in 10-100000, with 5000 points instead of 10000.
+## Order of the Bessel returned to 0, due to better agreement with data.
+## Reduced theta range to ~ 0.5-100 (it depends on the shift w.r.t l range) 
+## Introduced the limber correction term, with the possibility to switch it off.
+## Reduced the correction on the conversion factor.
+##################################################################
+
 import numpy as np
 import sys
 sys.path.insert(0, '/home/alessandro/code')
@@ -20,12 +30,11 @@ mean_path = inpath+'mean_DVs/kappa_2pcf/'
 outpath = 'Dustgrain_outs/'
 two_pt_out = outpath+'2PCF/'
 plot_out = outpath+'2PCF_plots/'
-# maps_out = plot_out+'2PCF_maps/'
 mean_out = plot_out+'2PCF_mean/'
-# lin_out = plot_out+'2PCF_lin/'
+lin_out = plot_out+'2PCF_lin/'
 vincenzo = outpath+'Vincenzo/2PCF/'
 os.makedirs(two_pt_out, exist_ok=True)
-# os.makedirs(maps_out, exist_ok=True)
+os.makedirs(lin_out, exist_ok=True)
 os.makedirs(mean_out, exist_ok=True)
 os.makedirs(vincenzo, exist_ok=True)
 
@@ -53,14 +62,14 @@ dl = np.log(l_array[1]/l_array[0]) # Discrete Hankel transform step
 # Settings for the FHT
 init = -1.2 # Initial step for the offset
 bs = -0.5 # Bias
-mu_j = 0 # Order of the ordinary Bessel function, not the spherical one
+mu_j = 0 # Order of the Bessel function
 conv_factor = 206265/60 # Conversion factor from radians to arcmins
-correction = np.sqrt(np.pi/2)*conv_factor**1.275 # Total correction to account for conversion and spherical Bessel
-# correction = np.sqrt(np.pi/2)*conv_factor**1.295
+correction_fr = np.sqrt(np.pi/2)*conv_factor**1.06 # Total correction to account for conversion and spherical Bessel
+correction_lcdm = np.sqrt(np.pi/2)*conv_factor**0.97
 offset = fft.fhtoffset(dl, initial=init, mu=mu_j, bias=bs) # Setting offset for low ringing condition
 theta = np.exp(offset)*conv_factor/l_array[::-1] # Output theta array
 
-# Set the Limber correction on/off
+# Switch the Limber correction on/off
 correct_limber = 1
 
 # Correction to the Limber approximation for low ls
@@ -127,6 +136,12 @@ for cosmo in cosmos:
         # The expression of the discrete Hankel transform has been modified to match
         # the canonical definition of the two-point c.f.
         print(f'Computing 2PCF for {cosmo} at zs={zs}\n')
+
+        if cosmo == 'lcdm':
+            correction = correction_lcdm
+        else:
+            correction = correction_fr
+
         xi_fft = fft.fht(cls*l_array*correction,dln=dl,mu=mu_j,offset=offset,bias=bs)/theta/2/np.pi
         # Saving the 2PCF values
         np.savetxt(two_pt_out+f'{cosmo}_{zs}_2pcf.txt',xi_fft)
@@ -135,31 +150,6 @@ for cosmo in cosmos:
         np.savetxt(vincenzo+f'{cosmo}_{zs}.txt',np.column_stack((np.log10(theta),xi_fft)),delimiter=',',header='log theta, xi(theta)')
 
         print(f'Saving plots for {cosmo} at zs={zs}\n')
-
-        # # Loading measured 2PCF DVs
-        # for map in np.linspace(100,255,10):
-        #     if cosmo == 'lcdm':
-        #         two_pt = np.loadtxt(dv_path+f'{round(map)}_LCDM_DUSTGRAIN_convergence_true_LCDM_z_{zs}_kappa_2pcf.txt',usecols=3)
-        #         xr = np.loadtxt(dv_path+f'{round(map)}_LCDM_DUSTGRAIN_convergence_true_LCDM_z_{zs}_kappa_2pcf.txt',usecols=1)
-        #     else:
-        #         two_pt = np.loadtxt(dv_path+f'{round(map)}_LCDM_DUSTGRAIN_convergence_true_'+ plot_label.replace(' ','_') + f'_z_{zs}_kappa_2pcf.txt',usecols=3)
-        #         xr = np.loadtxt(dv_path+f'{round(map)}_LCDM_DUSTGRAIN_convergence_true_'+ plot_label.replace(' ','_') + f'_z_{zs}_kappa_2pcf.txt',usecols=1)
-            
-        #     # Plotting and saving measurements/theory comparison for given maps
-        #     plt.figure(figsize=(8,6))
-        #     plt.scatter(xr,two_pt*xr**2,label='Measured',color='k')
-        #     plt.plot(theta,xi_fft*theta**2,label='Theory',color='r')
-        #     plt.xscale('log')
-        #     plt.yscale('log')
-        #     plt.xlim(twopt_min,twopt_max)
-        #     plt.title(rf'$\kappa$ - 2PCF, {plot_label}, $z_s$={zs}, {round(map)}',fontsize=16)
-        #     plt.xlabel(r'$\theta$ (arcmin)',fontsize=14)
-        #     plt.ylabel(r'$\theta^{2}\,\xi \, (\theta)$',fontsize=14)
-        #     plt.xticks([0.1,1,10,100,400],[0.1,1,10,100,400])
-        #     plt.legend()
-        #     plt.savefig(maps_out+f'{round(map)}_{cosmo}_{zs}.png', dpi=300) 
-        #     plt.clf()
-        #     plt.close('all')
         
         # Loading mean DVs
         if cosmo == 'lcdm':
@@ -173,33 +163,35 @@ for cosmo in cosmos:
 
         # Plotting and saving measurements/theory comparison for mean values
         plt.figure(figsize=(8,6))
-        plt.scatter(xr,two_pt*xr**2,label='Measured',color='k',marker='.')
-        plt.errorbar(xr,two_pt*xr**2,yerr=err*xr**2*2e5,color='k')
+        plt.scatter(xr,two_pt*xr**2,label='Measured',color='k',marker='o')
+        plt.errorbar(xr,two_pt*xr**2,yerr=err*1e4,fmt='none',color='k')
         plt.plot(theta,xi_fft*theta**2,label='Theory',color='r',linestyle='--')
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlim(0.5,50)
-        plt.ylim(1e-7,)
+        plt.xlim(.1,50)
+        plt.ylim(1e-6,)
         plt.title(rf'$\kappa$ - 2PCF, {plot_label}, Mean values, $z_s$={zs}',fontsize=16)
         plt.xlabel(r'$\theta$ (arcmin)',fontsize=14)
         plt.ylabel(r'$\theta^{2}\,\xi \, (\theta)$',fontsize=14)
-        plt.xticks([0.5,1,10,50],[0.5,1,10,50])
-        plt.legend()
+        plt.xticks([0.1,1,10,50],[0.1,1,10,50])
+        plt.legend(loc='lower right')
         plt.savefig(mean_out+f'{cosmo}_zs={zs}.png', dpi=300) 
         plt.clf()
         plt.close('all')
 
         # plt.figure(figsize=(8,6))
-        # plt.scatter(xr,two_pt,label='Measured',color='k')
-        # plt.plot(theta,xi_fft,label='Theory',color='r')
+        # plt.scatter(xr,two_pt,label='Measured',color='k',marker='o')
+        # plt.errorbar(xr,two_pt,yerr=err*1e3,fmt='none',color='k')
+        # plt.plot(theta,xi_fft,label='Theory',color='r',linestyle='--')
         # plt.xscale('log')
         # plt.yscale('log')
-        # plt.xlim(twopt_min,twopt_max)
+        # plt.xlim(0.1,50)
+        # plt.ylim(1e-6,)
         # plt.title(rf'$\kappa$ - 2PCF, {plot_label}, Mean values, $z_s$={zs}',fontsize=16)
         # plt.xlabel(r'$\theta$ (arcmin)',fontsize=14)
         # plt.ylabel(r'$\xi \, (\theta)$',fontsize=14)
-        # plt.xticks([0.1,1,10,100,400],[0.1,1,10,100,400])
-        # plt.legend()
+        # plt.xticks([0.1,1,10],[0.1,1,10])
+        # plt.legend(loc='upper right')
         # plt.savefig(lin_out+f'{cosmo}_zs={zs}_lin.png', dpi=300) 
         # plt.clf()
         # plt.close('all')
