b487449b843ed84602eb29ca82a39f596ce88a52
Alessandro Vadala <vadala.alessandro10@gmail.com>
2024-05-03 11:01:04 +0200

    Cls with all methods

--- dustgrain_cls.py
+++ dustgrain_cls.py
@@ -1,19 +1,12 @@
 import numpy as np
-import sys
-sys.path.insert(0, '/home/alessandro/code')
-
-from MGCAMB import camb
 
 from scipy import integrate, interpolate, constants as cs
-
-import matplotlib.pyplot as plt
 
 import csv
 
 import os
 
 from time import time
-
 
 # Define cosmological parameters for DUSTGRAIN-pathfinder simulations
 # as they appear in https://doi:10.1093/mnras/sty2465 for LCDM background
@@ -27,61 +20,73 @@ n_s = 0.9658
 w0 = -1.0
 w_a = 0.0
 
+# Define the range for k (just k, not k/h)
+k_min = 10**(-4.1) # for lower values CAMB does not compute P(z,k)
+k_max = 1000 # value high enough to safely use the interpolator
+
+# Define the redshift range
+z_range = np.linspace(0, 4, 100)
+# k_points = 300
+
 # DUSTGRAIN redshift values
 zs_values = [0.5, 1.0, 2.0, 4.0]
 
 # Number of values of ell to integrate over
 ell_points = 5000
 l_min = 1
-l_max = 4.61
+l_max = 5
+
+# Speed of light in vacuum in km/s
+c = cs.c/1e3
 
 # Cosmologies tag
 cosmos = ['lcdm', 'fr4','fr5', 'fr6', 'fr4_0.3', 'fr5_0.1', 'fr5_0.15', 'fr6_0.1', 'fr6_0.06']
 
-# Define output folder
+# Define input/output folder
 outpath = 'Dustgrain_outs/'
-plot_out = outpath+'Cls_plots/'
+cls_out = outpath+f'Cls/'
+pk_path = outpath+'Pknl/'
+
 print('Creating necessary directories\n')
+
 os.makedirs(outpath, exist_ok=True)
-os.makedirs(outpath+'Cls/', exist_ok=True)
-os.makedirs(plot_out, exist_ok=True)
+os.makedirs(cls_out, exist_ok=True)
 
 # directory for Vincenzo
 os.makedirs(outpath+'Vincenzo/Cls/', exist_ok=True)
 
-# Angular power spectrum without tomography
+# Dimensionless Hubble parameter
+def E(z):
+    return np.sqrt(Omega_M*(1+z)**3+Omega_DE)
+
+# Comoving radial distance
+def r(z):
+    integrand = lambda z_prime: 1 / E(z_prime)
+    result = integrate.quad(integrand, 0., z)[0]
+    return (c/H0)*result
+
+# Angular power spectrum
 def C_l(ell, zs):
     
-    c = cs.c/1e3 # in km/s
-    
-    def E(z): # dimensionless Hubble parameter
-        return res.hubble_parameter(z)/H0
-    
-    def r(z): # comoving radial distance in Mpc
-        return res.comoving_radial_distance(z)
-
     def W(z):
-        # Lensing efficiency for sources on the same plae
+        # Lensing efficiency for sources on the same plane
         return 1.5*Omega_M*(H0/c)**2*(1+z)*r(z)*(1-(r(z)/r(zs)))
 
     integrand = lambda z: W(z)**2 * P_zk(z, (ell+.5)/r(z)) / r(z)**2 / E(z)
     return (c/H0)*integrate.quad(integrand, 0., zs)[0]
 
-# Correction to the Limber approximation for low ls
-# def limber_correction(l):
-#     return (l+2)*(l+1)*l*(l-1)*(2/(2*l+1))**4
-# unused if l_min => 10
-
 t1 = time()
-
-kh_camb = np.loadtxt(outpath+f'/Pknl/lcdm_k.txt')
-z_camb = np.loadtxt(outpath+f'/Pknl/lcdm_z.txt')
 
 for cosmo in cosmos:
 
+    if cosmo == 'lcdm':
+        methods = ['HM', 'EE2', 'Win']
+    else:
+        methods = ['RHM','RHF', 'Win']
+
     print(f'Starting computation for {cosmo}\n')
 
-    # Density parameters and neutrino mass
+    # Density parameters and neutrino masses
 
     # LCDM parameters values
     if cosmo == 'lcdm':
@@ -185,62 +190,48 @@ for cosmo in cosmos:
 
         plot_label = 'fR6 0.06 eV'
 
-        # Value of fR0 for different MG models (must be absolute value)
-        if 'fr4' in cosmo:
-            fR0 = 1e-4
-        elif 'fr5' in cosmo:
-            fR0 = 1e-5
-        elif 'fr6' in cosmo:
-            fR0 = 1e-6
-    
-    with open(outpath+f'Pknl/'+cosmo.replace(' ','_')+'.txt','r',newline='\n') as file:
-        pk_nonlin = np.zeros((100,300))
-        reader = csv.reader(file)
-        for i, row in enumerate(reader):
-            pk_nonlin[i:] = row
+    # Omega Lambda for the selected cosmology
+    Omega_DE = 1-Omega_M-(Omega_nuh2/h**2)
+
+    for method in methods:
+
+        print(f'Importing power spectrum for {cosmo} with {method}\n')
+
+        k = np.loadtxt(pk_path+f'k_{cosmo}_{method}.txt')
+        z = np.loadtxt(pk_path+f'z_{cosmo}_{method}.txt')
+        pk_in = f'{cosmo}_{method}.txt'
+        plt_label = f'{method}'
         
-    # Setting a fixed grid interpolator to be able to use the Limber approximation
-    pk_interp = interpolate.RectBivariateSpline(z_camb, kh_camb, pk_nonlin, kx=5,ky=5)
+        with open(pk_path+pk_in,'r',newline='\n') as pk_impo:
+            pk_nonlin = np.zeros((len(z),len(k)))
+            reader = csv.reader(pk_impo)
+            for i,row in enumerate(reader):
+                pk_nonlin[i,:] = row
 
-    # Renaming the interpolator
-    P_zk = pk_interp
+        # Changing units from CAMB defaults
+        pk_nonlin = pk_nonlin/h**3
+        k = k*h
 
-    for zs in zs_values:
-        print(f'Computing C(l) for {cosmo} at zs={zs}\n')
-        # Setting the array for l with logarithically equispaced values
-        l_array = np.logspace(l_min,l_max,ell_points)
-        dl = np.log(l_array[1]/l_array[0]) # Discrete Hankel transform step
+        # Setting a fixed grid interpolator to be able to use the Limber approximation
+        pk_interp = interpolate.RectBivariateSpline(z, k, pk_nonlin, kx=5,ky=5)
 
-        # Compute the C(l)
-        # cl = np.fromiter((limber_correction(l)*C_l(l,zs) for l in l_array), float)
-        # only if l_min <= 10
-        cl = np.fromiter((C_l(l,zs) for l in l_array), float)
+        # Renaming the interpolator
+        P_zk = pk_interp
 
-        np.savetxt(outpath+rf'Cls/{cosmo}_{zs}.txt',cl)
+        for zs in zs_values:
 
-        # Plotting and saving C(l) plots
-        plt.figure(figsize=(8,6))
-        plt.loglog(l_array,l_array*(l_array+1)*cl/2/np.pi,label= rf'{plot_label}, $z_s={zs}$',color='k')
-        plt.xlim(l_array.min(),l_array.max())
-        plt.title(r'$\kappa$ - Angular power spectrum',fontsize=16)
-        plt.xlabel(r'$\ell$',fontsize=14)
-        plt.ylabel(r'$\ell \, (\ell + 1) \, P_{\kappa}(\ell) / (2\pi)$',fontsize=14)
-        plt.legend()
-        plt.savefig(plot_out+f'Cls_{cosmo}_zs={zs}.png', dpi=300) 
-        plt.clf()
-        plt.close('all')
-        
-        # for Vincenzo
-        np.savetxt(outpath+f'Vincenzo/Cls/{cosmo}_{zs}.txt',np.column_stack((np.log10(l_array),cl)),delimiter=',',header='log ell, C(ell)')
+            print(f'Computing C(l) for {cosmo} with {method} at zs={zs}\n')
+            # Setting the array for l with logarithically equispaced values
+            l_array = np.logspace(l_min,l_max,ell_points)
 
-    # Saving power spectra, k, and z arrays to file
-    with open(outpath+f'Pknl/{cosmo}.txt','w',newline='\n') as file:
-        writer = csv.writer(file)
-        writer.writerows(pk_nonlin*h**3)
+            # Compute the C(l)
+            cl = np.fromiter((C_l(l,zs) for l in l_array), float)
 
-    # np.savetxt(outpath+f'{cosmo}_Pk_nonlin.txt',pk_nonlin)
-    np.savetxt(outpath+f'/Pknl/{cosmo}_k.txt',kh_camb*h)
-    np.savetxt(outpath+f'/Pknl/{cosmo}_z.txt',z_camb)
+            # Saving on file
+            np.savetxt(cls_out+f'{cosmo}_{method}_{zs}.txt',cl)
+
+            # for Vincenzo
+            np.savetxt(outpath+f'Vincenzo/Cls/{cosmo}_{method}_{zs}.txt',np.column_stack((np.log10(l_array),cl)),delimiter=',',header='log ell, C(ell)')
 
 t2 = time()
 
